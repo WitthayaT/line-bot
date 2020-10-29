c4b46e3bb9aa46df310787488a67fd454762c14e
WitthayaT <70052065+WitthayaT@users.noreply.github.com>
2020-10-29 16:23:22 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -73,7 +73,7 @@ function mySQL_selectAll($url)
 	$data = "ผลลัพธ์:\r\n";
 		
 	foreach($result_json as $values) {
-		$data .= $values["stdid"] . " " . $values["fullname"] . "\r\n";
+		$data .= $values["stuid"] . " " . $values["fullname"] . "\r\n";
 	}
 	
 	return $data;
