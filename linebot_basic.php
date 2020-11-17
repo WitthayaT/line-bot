b344b1b6f20e48b01a3ff1fa0e85d73fc1f285b1
WitthayaT <70052065+WitthayaT@users.noreply.github.com>
2020-11-17 18:19:34 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -19,8 +19,8 @@ foreach ($request_json['events'] as $event)
  			//$reply_message = 'ฉันได้รับข้อความ '. $text.' ของคุณแล้ว!';   
 			//$reply_message ="Witthaya";
 			//$reply_message = mySQL_selectAll('http://s61160193.kantit.com/json_select.php');
-			if($devo == 'ใครคือผู้พัฒนา'){
-				$reply_message = "61160193 นายวิทยา เทพนวน";
+			if($devo[1] == 'ใครคือผู้พัฒนา'){
+				$reply_message = '61160193 นายวิทยา เทพนวน';
 			}else if($nameStd[1]){
 			$reply_message = selectStd($nameStd[1]);
 			}else if($lastname[1]){
